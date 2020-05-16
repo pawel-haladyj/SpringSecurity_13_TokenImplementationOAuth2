2e00dc0aed3b5ad3615e33073d470cb2659a43cc
Paweł Haładyj <pawel@haladyj.pl>
2020-05-16 11:01:37 +0200

    initial commit

---
+++ src/main/java/pl/haladyj/springsecurity13/SpringSecurity13Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity13Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/springsecurity13/SpringSecurity13ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
